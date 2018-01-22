982ade1f9f13faea736d568c7852940da57dab4d
jchiampi <jdc308@psu.edu>
2018-01-22 15:19:15 -0500

    Corrected SSL file path

--- public/deploymentServer.js
+++ public/deploymentServer.js
@@ -9,7 +9,7 @@ const https = require('https');
 const fs = require('fs');
 
 // Read SSL Files
-const cert = fs.readFileSync('science-olympiad/keys/IdeaProjects/cert.pem');
+const cert = fs.readFileSync('science-olympiad/keys/cert.pem');
 const key = fs.readFileSync('science-olympiad/keys/privatekey.pem');
 const ca = fs.readFileSync('science-olympiad/keys/chain.pem');
 
