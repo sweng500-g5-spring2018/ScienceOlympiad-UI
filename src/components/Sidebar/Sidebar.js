c2e144e032f84c48eaccbd24f4d390de15a60082
jchiampi <jdc308@psu.edu>
2018-01-26 21:40:06 -0500

    Merge branch 'master' of https://github.com/sweng500-g5-spring2018/ScienceOlympiad-UI into google_maps
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	src/components/app/App.js
    #	src/routes.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 import HeaderLinks from '../Header/HeaderLinks.js';
 
-import imagine from '../../assets/img/sidebar-3.jpg';
+import imagine from '../../assets/img/sidebar.jpg';
 import logo from '../../assets/img/reactlogo.png';
 
 import appRoutes from '../../routes/app.js';
