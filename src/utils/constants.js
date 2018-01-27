64e2ba68522f4ae6ba429434dfdf053750e1459c
ram757 <rmcdonald144@gmail.com>
2018-01-26 20:07:41 -0500

    adding missed files

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+
+module.exports.getServerUrl = function () {
+    if(process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'development') {
+        return 'http://localhost:8080/';
+    } else {
+        return window.location.protocol + '//server.sweng500.com/';
+    }
+}
+
+module.exports.isEmpty = function (obj) {
+    return Object.keys(obj).length === 0 && obj.constructor === Object;
+}
