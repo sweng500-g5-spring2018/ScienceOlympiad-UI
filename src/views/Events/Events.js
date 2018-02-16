18407379a65b9c3f9e850964f3b8fa64d41ae1e5
Kyle Hughes <kyle.hughes025@gmail.com>
2018-02-16 18:31:52 -0500

    More layout things with events #SO_API-30

--- src/views/Events/Events.js
+++ src/views/Events/Events.js
@@ -17,6 +17,11 @@ import DatePicker from 'material-ui/DatePicker'
 import TimePicker from 'material-ui/TimePicker'
 import AppBar from 'material-ui/AppBar'
 import {blue500} from 'material-ui/styles/colors'
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import FlatButton from 'material-ui/FlatButton';
+import FontIcon from 'material-ui/FontIcon';
+
 
 class Events extends Component {
     constructor(props) {
@@ -24,6 +29,8 @@ class Events extends Component {
         this.closeModal = this.closeModal.bind(this);
         this.createNewEvent = this.createNewEvent.bind(this);
         this.createEventPost = this.createEventPost.bind(this);
+        this.nextStep = this.nextStep.bind(this);
+        this.previousStep = this.previousStep.bind(this);
 
         this.state = {
             test: {},
@@ -52,11 +59,38 @@ class Events extends Component {
     createEventPost() {
         alert(this.state.eventName + " --- " + this.state.eventDate);
         alert("make ajax call to create the event from the fields");
+        //show some success and then clear the fields
+        this.setState({
+            modal: false,
+            stepIndex: 0,
+
+            eventName: '',
+            //this is a date object
+            eventDate: '',
+            startTime: '',
+            endTime: ''
+
+        })
     }
 
     closeModal() {
         this.setState({
             modal: false
+        })
+    }
+
+    nextStep() {
+        //validate entries here
+        const {stepIndex} = this.state;
+        this.setState({
+            stepIndex: stepIndex + 1
+        })
+    }
+
+    previousStep() {
+        const {stepIndex} = this.state;
+        this.setState({
+            stepIndex: stepIndex - 1
         })
     }
 
@@ -83,40 +117,36 @@ class Events extends Component {
         if (this.state.test !== null && Object.keys(this.state.test).length !== 0) {
 
             return (
+                <Col md={12}>
+                    <div>
+                        <Table striped bordered condensed hover>
+                            <thead>
+                            <tr>
+                                <th>Event Name</th>
+                                <th>Event Date</th>
+                            </tr>
+                            </thead>
+                            <tbody>
 
-                <div>
+                            {
+                                Object.keys(this.state.test).map(function (key) {
+                                    //if(key === 'name' || key === 'id') {
+                                    return (
+                                        <tr>
+                                            <td>{this.state.test[key].name} </td>
+                                            <td>TBD</td>
+                                        </tr>
+                                    )
+                                    // }
+                                }, this)
 
+                            }
 
-                    <Table striped bordered condensed hover>
-                        <thead>
-                        <tr>
-                            <th>Event Name</th>
-                            <th>Event Date</th>
-                        </tr>
-                        </thead>
-                        <tbody>
+                            </tbody>
+                        </Table>
 
-
-                        {
-
-
-                            Object.keys(this.state.test).map(function (key) {
-                                //if(key === 'name' || key === 'id') {
-                                return (
-                                    <tr>
-                                        <td>{this.state.test[key].name} </td>
-                                        <td>TBD</td>
-                                    </tr>
-                                )
-                                // }
-                            }, this)
-
-                        }
-
-                        </tbody>
-                    </Table>
-
-                </div>
+                    </div>
+                </Col>
             )
         } else {
 
@@ -132,24 +162,40 @@ class Events extends Component {
                 width: '100%'
             }
         };
+        let actionButton = null;
+        let backButton = null;
+        //control which buttons show up in the modal
+        if (this.state.stepIndex == 0) {
+            actionButton = <RaisedButton icon={<FontIcon className="pe-7s-angle-down-circle" />} primary={true} label="Judges"
+                                         onClick={this.nextStep}/>;
+        } else {
+            backButton = <RaisedButton icon={<FontIcon className="pe-7s-angle-up-circle" />}primary={true} label="Back to Info"
+                                       onClick={this.previousStep}/>;
+            actionButton = <RaisedButton icon={<FontIcon className="pe-7s-like2" />} primary={true} label="Create Event"
+                                         onClick={this.createEventPost}/>;
+        }
         return (
 
             <div className="content">
                 <div key="notFound-key" className="notFoundClass">
                     <MuiThemeProvider>
                         <Grid>
-                            <Row>
+                            <Row className="show-grid">
                                 <Col md={4} mdOffset={4}>
                                     <RaisedButton primary={true} label="Create New Event"
                                                   onClick={this.createNewEvent}/>
                                     <br/>
                                 </Col>
                             </Row>
-                            <Row>
+                            <br/>
+                            <br/>
+                            <Row className="show-grid">
+
                                 <Loader color="#3498db" loaded={this.state.loading}>
 
                                     {this.renderIfTestFound()}
                                 </Loader>
+
                             </Row>
                         </Grid>
                     </MuiThemeProvider>
@@ -157,9 +203,11 @@ class Events extends Component {
                 </div>
                 <MuiThemeProvider>
                     <Modal show={this.state.modal} onHide={this.closeModal}>
-                        <Modal.Header closeButton>
+                        <Modal.Header>
                             <Modal.Title> <AppBar
+                                iconElementRight={<FlatButton label="Close"/>}
                                 showMenuIconButton={false}
+                                onRightIconButtonClick={(event) => this.closeModal()}
                                 title="Create New Event"
                             /></Modal.Title>
                         </Modal.Header>
@@ -172,7 +220,7 @@ class Events extends Component {
                                         <StepContent>
 
                                             <Row className="show-grid">
-                                                <Col lg={2} md={2} mdOffset={1} xs={2}>
+                                                <Col md={2} xs={2}>
                                                     <TextField
                                                         id={"eventName"}
                                                         floatingLabelText="Event Name"
@@ -181,19 +229,16 @@ class Events extends Component {
                                                         required={true}
                                                         autoFocus={true}
                                                         fullWidth={true}
-                                                        inputStyle={styles.formFields}
                                                     />
                                                 </Col>
-                                            </Row>
-                                            <br/>
-                                            <Row className="show-grid">
-                                                <Col lg={1} md={1} mdOffset={1} xs={4}>
+                                                <Col md={2} mdOffset={1} xs={2}>
                                                     <DatePicker
-                                                        inputStyle={styles.formFields}
+                                                        // inputStyle={styles.formFields}
                                                         onChange={(event, newValue) => this.setState({eventDate: newValue})}
                                                         value={this.state.eventDate}
                                                         hintText="Event Date"
-                                                        hintStyle={styles.formFields}
+                                                        floatingLabelText="Event Date"
+                                                        //hintStyle={styles.formFields}
                                                         textFieldStyle={styles.formFields}
 
                                                         mode="landscape"/>
@@ -202,8 +247,13 @@ class Events extends Component {
                                             </Row>
                                             <br/>
                                             <Row className="show-grid">
-                                                <Col md={1} xs={3}>
+
+                                            </Row>
+                                            <br/>
+                                            <Row className="show-grid">
+                                                <Col md={2} xs={3}>
                                                     <TimePicker
+                                                        floatingLabelText="Start Time"
                                                         onChange={(event, newValue) => this.setState({startTime: newValue})}
                                                         value={this.state.startTime}
                                                         textFieldStyle={styles.formFields}
@@ -211,8 +261,9 @@ class Events extends Component {
                                                         autoOk={true}
                                                     />
                                                 </Col>
-                                                <Col md={1} mdOffset={3} xs={3}>
+                                                <Col md={2} mdOffset={1} xs={3}>
                                                     <TimePicker
+                                                        floatingLabelText="End Time"
                                                         onChange={(event, newValue) => this.setState({endTime: newValue})}
                                                         value={this.state.endTime}
                                                         hintText="End time"
@@ -235,8 +286,7 @@ class Events extends Component {
                         </Modal.Body>
 
                         <Modal.Footer>
-                            <RaisedButton primary={true} label="Create Event"
-                                          onClick={this.createEventPost}/>,
+                            {backButton} {actionButton}
                         </Modal.Footer>
                     </Modal>
 
