6c3ddda854ea462ee524d61efa32b97e1a138b01
ram757 <rmcdonald144@gmail.com>
2018-03-04 13:28:00 -0500

    #SO_UI-11: adding tests to Header.js

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,73 @@
+//React / Testing frameworks
+import React from 'react';
+import sinon from 'sinon';
+import {expect} from 'chai';
+import {shallow, mount} from 'enzyme';
+
+// jest.mock('react-router-dom');
+
+/* Test Helpers */
+import helper from '../../../test/helpers/helper';
+
+//CHILDREN COMPONENTS OF COMPONENT IN TEST
+import { Navbar } from 'react-bootstrap';
+import HeaderLinks from './HeaderLinks.js';
+import appRoutes from '../../routes/app.js';
+
+//COMPONENT TO TEST
+import Header from './Header';
+
+describe('Header Component Tests', function () {
+
+    const notify = sinon.spy();
+    const reactRouterData = require('../../../test/data/header/reactRouterTest');
+    const mockedEvent = { target: {}, preventDefault : () => {} }
+
+    //Set up test data before running any tests
+    beforeAll(function () {
+    })
+
+    afterEach(function () {
+    })
+
+    // Test 1
+    test('Should render Header component with React Bootstrap Navbar components', () => {
+        const component = shallow(<Header {... reactRouterData} />);
+
+        expect(component.find(Navbar)).to.have.length(1);
+        expect(component.find(Navbar.Header)).to.have.length(1);
+        expect(component.find(Navbar.Brand)).to.have.length(1);
+        expect(component.find(Navbar.Toggle)).to.have.length(1);
+        expect(component.find(Navbar.Collapse)).to.have.length(1);
+
+        expect(component.find(HeaderLinks)).to.have.length(1);
+
+    })
+
+    // Test 2
+    test('Should render div containing the result of getBrand() which should equal "Dashboard"', () => {
+        const component = shallow(<Header {... reactRouterData} />);
+
+        let brandDisplay = component.find('div#brand-display');
+        expect(brandDisplay).to.have.length(1);
+        expect(brandDisplay.text()).to.equal("Dashboard");
+    })
+
+    // Test 3
+    test('Should render Navbar and run function to handle mobileSidebarToggle event', async () => {
+        const component = shallow(<Header {... reactRouterData} />);
+
+        let navbarToggle = component.find(Navbar.Toggle);
+        expect(navbarToggle).to.have.length(1);
+
+        expect(component.instance().state.sidebarExists).to.equal(false);
+
+        expect(navbarToggle.simulate('click', mockedEvent));
+
+        await helper.flushPromises();
+        component.update();
+
+        expect(component.instance().state.sidebarExists).to.equal(true);
+    })
+
+});
