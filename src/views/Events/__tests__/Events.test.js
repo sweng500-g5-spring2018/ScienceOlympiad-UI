5a53b15c01c4c40a34d08fcbb7971ac049358d9f
Kyle Hughes <kyle.hughes025@gmail.com>
2018-03-16 10:05:14 -0400

    Event Tests

--- src/views/Events/__tests__/Events.test.js
+++ src/views/Events/__tests__/Events.test.js
@@ -23,6 +23,9 @@ import RaisedButton from "material-ui/RaisedButton/index";
 import Dialog from 'material-ui/Dialog';
 import {DatePicker, TimePicker} from "material-ui";
 import BuildingSelector from "../../../components/Buildings/BuildingSelector";
+import ReactTable from 'react-table'
+import EventDetail from "../EventDetail";
+
 
 describe('Event Component Tests', function () {
 
@@ -58,6 +61,7 @@ describe('Event Component Tests', function () {
         component.update();
 
         expect(component.state().events.length).to.equal(2);
+        expect(component.find(ReactTable)).to.have.length(1);
     });
 
     // Test 2
@@ -97,5 +101,79 @@ describe('Event Component Tests', function () {
         expect(component.find(DatePicker)).to.have.length(1);
         expect(component.find(TimePicker)).to.have.length(2);
         expect(component.find(BuildingSelector)).to.have.length(1);
+    });
+
+    //test 4
+    test('Test deleting a school', async () => {
+
+        //S imulate the user be logged on
+        sinon.stub(AuthService, 'isLoggedIn').returns(true)
+
+        const component = shallow(<Events/>);
+
+        component.instance().addNotification = sinon.spy();
+
+        // Setup a fake school to delete
+       // component.instance().setState({deleteID: '1'})
+
+        // Wait for setState's to finish and re-render component
+        await helper.flushPromises();
+        component.update();
+
+        // Simulate opening the delete confirmation, which sets deleteID state
+        var s = [];
+        s.ID = 1;
+        s.name = "Test";
+        component.instance().confirmEventDelete(s);
+
+        // Checks to see if the dialog opened
+        expect(component.state().confirmDialog).to.equal(true);
+
+        // Simulate the confirm delete function opening
+        component.instance().removeEvent();
+
+        // Wait for setState's to finish and re-render component
+        await helper.flushPromises();
+        component.update();
+
+        // Check notification to make sure the delete worked
+        expect(component.instance().addNotification.getCall(0).args[0]).to.equal("Success: The event has been deleted.");
+    });
+
+    // Test 5
+    test('Make sure action buttons are rendered in table', async () => {
+
+        //Simulate the user be logged on
+        sinon.stub(AuthService, 'isLoggedIn').returns(true)
+
+        //load full component?
+        const component = mount(<Events />);
+
+        //Wait for setState's to finish and re-render component
+        await helper.flushPromises();
+        component.update();
+
+        //two fake events loaded so 2 actions for each and 1 create event button
+
+        expect(component.find(RaisedButton)).to.have.length(5);
+    });
+
+    // Test 6
+    test('Simulate opening an event details', async () => {
+
+        //Simulate the user be logged on
+        sinon.stub(AuthService, 'isLoggedIn').returns(true)
+
+        //load full component?
+        const component = mount(<Events />);
+
+        //simulate loading in the event details
+        component.instance().eventDetails('1');
+
+        //Wait for setState's to finish and re-render component
+        await helper.flushPromises();
+        component.update();
+
+        expect(component.find(EventDetail)).to.have.length(1);
     });
 });
